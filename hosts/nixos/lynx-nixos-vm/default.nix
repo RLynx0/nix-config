974d19ec3830beca37baffb3c039e0235e30560b
RLynx <luc.signin96@gmail.com>
2025-11-23 22:22:43 +0100

    Use nftables plz

--- hosts/nixos/lynx-nixos-vm/default.nix
+++ hosts/nixos/lynx-nixos-vm/default.nix
@@ -45,6 +45,9 @@
   services.fail2ban.enable = true;
   security.apparmor.enable = true;
   security.rtkit.enable = true;
+
+  ## Firewall
+  networking.nftables.enable = true;
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 22 ];
