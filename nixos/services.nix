95fa7cc376fe7d605ea579169be006eb23b260c6
RLynx0 <luc.signin96@gmail.com>
2026-07-13 01:35:38 +0200

    Added support for HP ENVY printer

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.locate.enable = true;
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.hplipWithPlugin ];
+  };
   services.hardware.bolt.enable = true;
 }
