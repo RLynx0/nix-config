b4ead61f8c9f6f2cf3e253e8823cead937569a20
RLynx0 <luc.signin96@gmail.com>
2026-07-27 19:57:14 +0200

    Added overlay for lact (again?)

--- modules/hosts/lynxLaptop/default.nix
+++ modules/hosts/lynxLaptop/default.nix
@@ -4,6 +4,18 @@
   flake.nixosConfigurations.lynx-laptop = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       ../../../hosts/lynxLaptop/default.nix
+      # Workaround: lact-0.9.1 requires libdisplay-info < 0.4.0, but nixpkgs-unstable
+      # bumped it to 0.4.0. Use the version from nixpkgs-stable (0.3.x) until fixed upstream.
+      (_final: {
+        nixpkgs.overlays = [
+          (_: prev: {
+            lact = prev.lact.override {
+              libdisplay-info =
+                inputs.nixkgs-stable.legacyPackages.${prev.stdenv.hostPlatform.system}.libdisplay-info;
+            };
+          })
+        ];
+      })
       inputs.home-manager.nixosModules.home-manager
       {
         home-manager.useGlobalPkgs = true;
